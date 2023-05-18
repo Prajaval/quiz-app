a1c47dff31c9277a3d40792192886c6c0bc8e402
Prajaval Choudhary <93463237+Prajaval@users.noreply.github.com>
2023-05-18 22:49:23 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Quiz-App
+
+## Application Link-> https://febiecode.github.io/Quiz-App/
+
+# JavaScript Quiz Game Layouts
+
+![download](https://user-images.githubusercontent.com/93641901/194402875-8e729401-36cd-46d3-927c-522b6ecdbe34.png)
+![download](https://user-images.githubusercontent.com/93641901/194402955-22fda31f-fecb-46eb-8438-1fca44648666.png)
+![download](https://user-images.githubusercontent.com/93641901/194403053-44d75de9-9000-4115-8c46-41020ccfe2ed.png)
+![download](https://user-images.githubusercontent.com/93641901/194579061-ca62c3e0-94bc-4856-a624-ef5681ff9e1a.png)
+![download](https://user-images.githubusercontent.com/93641901/194403366-3c82662b-e742-498e-a3bb-b4fe8e0494ba.png)
+![download](https://user-images.githubusercontent.com/93641901/194403176-23fe98d6-103b-44be-aa66-a3f2c627b3b0.png)
+![download](https://user-images.githubusercontent.com/93641901/194403501-af0df723-cedf-467e-9dc2-e6ada803d53d.png)
+
---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "django is written in which language?",
+    answer: "Python",
+    options: [
+      "Python",
+      "Java",
+      "PHP",
+      "C Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Django is a type of",
+    answer: "Web framework",
+    options: [
+      "Tool",
+      "Software",
+      "Web framework",
+      "Programming Language"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Django is a _____________ Python web framework.",
+    answer: "High-Level",
+    options: [
+      "High-Level",
+      "Mid-Level",
+      "Low-Level",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which architectural pattern does django follow?",
+    answer: "MVT",
+    options: [
+      "CSS",
+      "MVC",
+      "MVT",
+      "PHP"
+    ]
+  },
+    {
+    numb: 5,
+    question: "MVT Stands for?",
+    answer: "Model-View-Template",
+    options: [
+      "Model-View-Table",
+      "Map-View-Template",
+      "Model-View-Template",
+      "Main-View-Template"
+    ]
+  },
+];
